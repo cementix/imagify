f251470064c6b66f706bff34986d310993b3079c
cementix <bodakondrackij@gmail.com>
2025-09-28 18:43:36 +0200

    first commit

---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { AppContext } from "./AppContext";
+
+const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const value = { user, setUser };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export default AppContextProvider;
