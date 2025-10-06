d9e30d713d92ad31e1641aa30aca8270b6786608
cementix <bodakondrackij@gmail.com>
2025-10-06 14:05:29 +0200

    full client pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext.jsx";
 
 const Navbar = () => {
-  const { user } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
   const navigate = useNavigate();
 
   return (
@@ -44,7 +44,10 @@ const Navbar = () => {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white py-2 px-7 sm:px-10 text-sm rounded-full">
+            <button
+              onClick={() => setShowLogin(true)}
+              className="bg-zinc-800 cursor-pointer text-white py-2 px-7 sm:px-10 text-sm rounded-full"
+            >
               Login
             </button>
           </div>
--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { AppContext } from "./AppContext";
 
 const AppContextProvider = ({ children }) => {
-  const [user, setUser] = useState(true);
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
 
-  const value = { user, setUser };
+  const value = { user, setUser, showLogin, setShowLogin };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
