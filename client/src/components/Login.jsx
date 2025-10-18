4bdc7e223a5ff143c331737dc7dd4d4dc90302b3
cementix <bodakondrackij@gmail.com>
2025-10-18 10:55:54 +0200

    login handler + local storage token

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,58 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [state, setState] = useState("Login");
-  const { setShowLogin } = useContext(AppContext);
+  const { setShowLogin, backendUrl, setToken, setUser } =
+    useContext(AppContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (state === "Login") {
+        const { data } = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+          localStorage.setItem("token", data.token);
+          setShowLogin(false);
+          toast.success(`Successfully logged in`);
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setToken(data.token);
+          setUser(data.user);
+          localStorage.setItem("token", data.token);
+          setShowLogin(false);
+          toast.success(`Successfully registered`);
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
@@ -16,7 +64,10 @@ const Login = () => {
 
   return (
     <div className="fixed inset-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <form
+        onSubmit={onSubmitHandler}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -26,6 +77,8 @@ const Login = () => {
           <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
             <img src={assets.profile_icon} width={16} alt="" />
             <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
               type="text"
               className="outline-none text-sm"
               placeholder="Full Name"
@@ -37,6 +90,8 @@ const Login = () => {
         <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
           <img src={assets.email_icon} width={16} alt="" />
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             type="email"
             className="outline-none text-sm"
             placeholder="Email"
@@ -47,6 +102,8 @@ const Login = () => {
         <div className="border px-6 py-2 flex items-center gap-2 rounded-full mt-5">
           <img src={assets.lock_icon} width={11} alt="" />
           <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
             type="password"
             className="outline-none text-sm"
             placeholder="Password"
